b97b71cc7ac7b83f0213229b76f55dd1f45013e8
NwanetiUche <167586801+NwanetiUche@users.noreply.github.com>
2024-06-02 18:59:26 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+alert("Thank you for giving me a chance");
+function myHTML(){
+    show("html");
+}	
+
+function myCSS(){
+  show("css");
+}
+function myJavascript(){
+  show("javascript");
+}
+
+function show(Id){document.getElementById(Id).style.display="block";}
